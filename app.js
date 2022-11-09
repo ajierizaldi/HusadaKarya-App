3be01b9819a3b83779dd40c68a34cc98e9942f72
Muhammad Ajie Rizaldi <ajiemuhammad2@gmail.com>
2022-11-10 03:51:17 +0700

    initial init

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// const router = require('./routes');
+const ejs = require('ejs');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+// app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
